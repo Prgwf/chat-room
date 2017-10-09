703a271df48da2ada3b3bce0fd4d7f7055386775
Prgwf <peter.wfgu@gmail.com>
2017-10-10 00:41:37 +0800

    version0.1

--- client.py
+++ client.py
@@ -1,17 +1,54 @@
 # -*- coding: utf-8 -*-
-import sys, time, socket
+import sys, time, socket, threading
 from ui import Ui_MainWindow
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui, QtCore
+
+server_host = '127.0.0.1'
+server_port = 8989
 
 class Client(object):
     def __init__(self):
         self.App = QtWidgets.QApplication(sys.argv)
         self.Window = Ui_MainWindow()
         self.Window.sendbutton.clicked.connect(self.send_button)
-
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((server_host, server_port))
+        self.message_recv()
 
     def send_button(self):
-        pass
+        try:
+            self.name = self.Window.namejar.text().strip()
+            self.message = self.Window.msgjar.text()
+            if self.name == '':
+                QtWidgets.QMessageBox.critical(QtWidgets.QMessageBox(), 'error', 'please input nickname')
+                return
+            elif self.message == '':
+                QtWidgets.QMessageBox.critical(QtWidgets.QMessageBox(), 'error', 'please input your message')
+                return
+
+            self.socket.send(bytes(self.name + '|' + self.message, 'utf-8'))
+            self.Window.msgjar.setText('')
+        except BaseException as err:
+            QtWidgets.QMessageBox.critical(QtWidgets.QMessageBox(), 'error', str(err))
+
+    def message_recv(self):
+        def recv_help():
+            model = QtGui.QStandardItemModel(self.Window.msglist)
+            while True:
+                try:
+                    msg = self.socket.recv(2048).decode('utf-8')
+                    t = time.strftime('%Y-%m-%d %H:%M:%S\t', time.localtime())
+                    msg = t + ' ' + msg
+                    raw = QtGui.QStandardItem()
+                    raw.setText(msg)
+                    model.appendRow(raw)
+                    self.Window.msglist.setModel(model)
+                    self.Window.msglist.update()
+                except socket.error:
+                    break
+        sub_thread = threading.Thread(target=recv_help, args=())
+        sub_thread.setDaemon(True)
+        sub_thread.start()
 
 if __name__ == '__main__':
     t = Client()
--- server.py
+++ server.py
@@ -9,23 +9,42 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((default_host, default_port))
 server_socket.listen(10)
 
-def broadcast(message):
-    pass
+client_list = []
 
-def tcplink(sock, addr):
-    print('Accept new connection from %s:%s...' % addr)
-    sock.send(b'Welcome!')
+def client_thread(conn, addr):
     while True:
-        data = sock.recv(1024)
-        time.sleep(1)
-        if not data or data.decode('utf-8') == 'exit':
-            break
-        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
-    sock.close()
-    print('Connection from %s:%s closed.' % addr)
+        try:
+            raw = conn.recv(2048)
+            # debug why server no response
+            # print(raw)
+            if raw:
+                raw = raw.decode('utf-8').split('|')
+                message = '<' + raw[0] + '>: ' + raw[1]
+                # print(raw[0], '\n', raw[1])
+                broadcast(message, conn)
+                conn.send(bytes('<You>: ' + raw[1], 'utf-8'))
+            else:
+                remove(conn)
+        except:
+            continue
+
+def broadcast(message, connection):
+    for client in client_list:
+        if client != connection:
+            try:
+                client.send(bytes(message, 'utf-8'))
+            except:
+                client.close()
+                remove(client)
+
+def remove(connection):
+    if connection in client_list:
+        client_list.remove(connection)
 
 
-while True:
-    conn, addr = server_socket.accept()
-    t = threading.Thread(target = tcplink, args = (conn, addr))
-    t.start()
+if __name__ == '__main__':
+    while True:
+        conn, addr = server_socket.accept()
+        client_list.append(conn)
+        t = threading.Thread(target = client_thread, args = (conn, addr))
+        t.start()
