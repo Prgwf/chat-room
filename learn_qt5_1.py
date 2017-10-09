cec92d565cd99770291dfee8f7135962db9cbd0c
Prgwf <peter.wfgu@gmail.com>
2017-10-09 20:51:40 +0800

    rewrite

--- learn_qt5_1.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5 import QtWidgets
-
-app = QtWidgets.QApplication(sys.argv)
-
-label = QtWidgets.QLabel("<p style='color: blue; margin-left: 20px'><b>hell world</b></p>")
-label.show()
-
-sys.exit(app.exec_())
